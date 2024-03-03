33a40497d72865d2fa40b00bbaca2476c65a1fca
mangaldeepak <deepakkandir7050@gmail.com>
2024-03-03 23:27:38 +0530

    updatatin

--- practice.js
+++ practice.js
@@ -1,26 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var countdownElement = document.getElementById('countdown');
-    var birthdayMessageElement = document.getElementById('birthday-message');
-
-    function updateCountdown() {
-        var now = new Date();
-        var tomorrow = new Date(now);
-        tomorrow.setDate(now.getDate() + 1);
-        tomorrow.setHours(0, 0, 0, 0);
-
-        var timeUntilBirthday = tomorrow - now;
-
-        var days = Math.floor(timeUntilBirthday / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((timeUntilBirthday % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((timeUntilBirthday % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((timeUntilBirthday % (1000 * 60)) / 1000);
-
-        countdownElement.innerHTML = days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's';
-
-birthdayMessageElement.innerHTML = '“Be happy! Today is the day you were brought into this world to be a blessing and inspiration to the people around you! You are a wonderful person! May you be given more birthdays to fulfill all of your dreams!”';
-
-}
-
-    updateCountdown();
-    setInterval(updateCountdown, 1000);
-});
+class Car {
+    constructor(name, year) {
+      this.name = name;
+      this.year = year;
+    }
+    age() {
+      const date = new Date();
+      return date.getFullYear() - this.year;
+    }
+  }
+  
+  const myCar = new Car("Ford", 2014);
+  document.getElementById("demo").innerHTML =
+  "My car is " + myCar.age() + " years old.";
+  
+  
