3d3f92a98770c90405dbc25db9d03a9f1073eb9e
mangaldeepak <deepakkandir7050@gmail.com>
2024-03-03 21:20:43 +0530

    jlgsdjgasldg

--- practice.js
+++ practice.js
@@ -1,19 +1,26 @@
-class car{
-constructor(brand){
-    this.carname=brand;}
-present(brand) {
-return 'i have a'+this.carname;
+document.addEventListener('DOMContentLoaded', function () {
+    var countdownElement = document.getElementById('countdown');
+    var birthdayMessageElement = document.getElementById('birthday-message');
+
+    function updateCountdown() {
+        var now = new Date();
+        var tomorrow = new Date(now);
+        tomorrow.setDate(now.getDate() + 1);
+        tomorrow.setHours(0, 0, 0, 0);
+
+        var timeUntilBirthday = tomorrow - now;
+
+        var days = Math.floor(timeUntilBirthday / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((timeUntilBirthday % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((timeUntilBirthday % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((timeUntilBirthday % (1000 * 60)) / 1000);
+
+        countdownElement.innerHTML = days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's';
+
+birthdayMessageElement.innerHTML = '“Be happy! Today is the day you were brought into this world to be a blessing and inspiration to the people around you! You are a wonderful person! May you be given more birthdays to fulfill all of your dreams!”';
+
 }
-}
- 
-class model extends car{
-    constructor(brand,mod){
-super(brand);
-this.model=mod;
-    }
-    show(){
-        return this.present()+',it is a  '+this.model;
-    }
-}
-const mycar=new  model("ford","mustang");
-document.getElementById("demo").innerHTML=mycar.show();
+
+    updateCountdown();
+    setInterval(updateCountdown, 1000);
+});
